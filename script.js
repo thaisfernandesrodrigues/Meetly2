081a2c45bf8db58d1a35847f3d29a5a99b202154
Thais Fernandes <thaisfernandesro@gmail.com>
2026-05-27 09:33:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ botao.addEventListener('click', function() {
   
   /*aqui você pode colocar qualquer lógica antes de ir para a página!*/
   console.log("O usuário clicou! Redirecionando...");
-  
+
   /* faz o navegador mudar de página */
   window.location.href = "pag2.html";
 });
